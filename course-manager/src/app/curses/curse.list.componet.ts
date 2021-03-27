66ba2b88a83dffcdad620bccb89b182f5a8def34
otaviov <amandavv32@gmail.com>
2021-03-27 20:41:11 -0300

    curse.list.componet.html

--- course-manager/src/app/curses/curse.list.componet.ts
+++ course-manager/src/app/curses/curse.list.componet.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-course-list',
-    template: '<h2> Course List </h2>'
+    templateUrl: './curse.list.componet.html'
 
 })
 
