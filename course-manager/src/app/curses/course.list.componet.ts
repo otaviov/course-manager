be24f7b8d5ceafb992cfac1820fa8e5692875b4c
otaviov <amandavv32@gmail.com>
2021-03-31 15:58:24 -0300

    filter

--- course-manager/src/app/curses/course.list.componet.ts
+++ course-manager/src/app/curses/course.list.componet.ts
@@ -9,12 +9,27 @@ import { CourseService } from './course.service';
 
 export class CourseListComponent implements OnInit{
 
-    courses: Course[] = [];
+    filteredCourses: Course[] = [];
+
+    _courses: Course[] = [];
+
+    
+    _filterby: string;
 
     constructor(private courseService: CourseService) { }
 
     ngOnInit(): void {
-        this.courses = this.courseService.retrieveAll();
+        this._courses = this.courseService.retrieveAll();
+        this.filteredCourses = this._courses;
+    }
+
+    set filter(value: string){
+        this._filterby = value;
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterby.toLocaleLowerCase()) > -1);
+    }
+
+    get filter(){
+        return this._filterby;
     }
 
 }
