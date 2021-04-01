a2748d97a7f13343016d125d7c31c085d811589e
otaviov <amandavv32@gmail.com>
2021-03-31 22:38:07 -0300

    ativando rotas para acessar componentes

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -3,6 +3,7 @@ import {FormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { CourseInfoComponent } from './curses/course-info.component';
 import { CourseListComponent } from './curses/course.list.componet';
 import { StarComponent } from './curses/star/star.component';
 import { Error404Component } from './error-404/error-404.component';
@@ -16,7 +17,8 @@ import { ReplacePipe } from './pipe/replace.pipe';
     StarComponent,
     ReplacePipe,
     NavBarComponent,
-    Error404Component
+    Error404Component,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule, 
@@ -26,9 +28,12 @@ import { ReplacePipe } from './pipe/replace.pipe';
         path: '', redirectTo: 'courses', pathMatch: 'full'
       },
       {
+        path: 'courses/info/:id', component: CourseInfoComponent
+
+      },
+      {
         path: 'courses', component: CourseListComponent
       },
-
       {
         path: '**', component: Error404Component
       }
