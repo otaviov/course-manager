66150d56ca2ce7de34029a106354c8ebbe06297f
otaviov <amandavv32@gmail.com>
2021-03-31 22:05:27 -0300

    error

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './curses/course.list.componet';
 import { StarComponent } from './curses/star/star.component';
+import { Error404Component } from './error-404/error-404.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 
@@ -14,10 +15,24 @@ import { ReplacePipe } from './pipe/replace.pipe';
     CourseListComponent,
     StarComponent,
     ReplacePipe,
-    NavBarComponent
+    NavBarComponent,
+    Error404Component
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      },
+
+      {
+        path: '**', component: Error404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
